04c8e6451ef04bc72fd8ae87b5605fa86bd75096
Vladislav <komarov_v_y@ngs.ru>
2021-02-20 17:39:13 +0700

    Code ger_move(), add check to set_field()

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,5 @@
-player_sign = ('x', 'o')
+PLAYER_SIGN = ('x', 'o')
+ATTEMPT_MAX = 3
 
 def show_field():
     print("  0 1 2")
@@ -9,10 +10,24 @@ def line_is_done():
     return True
 
 def get_move():
-    return 0, 0
+    attempt_count = 0
+    while attempt_count < ATTEMPT_MAX:
+        row, col = tuple(map(int, (input("Enter row and column numbers (example: 1 2):").split())))
+        if all([0 <= row < 3, 0 <= col < 3, ttt_field[row][col] == '-']):
+            return (row, col)
+        if not all([0 <= row < 3, 0 <= col < 3]):
+            print("Row and column numbers should be in 0..2 range!")
+        if ttt_field[row][col] != '-':
+            print(f"Field {row},{col} is already filled!")
+    print(f"Your choise {row},{col}")
+    return row, col
 
 def set_field(coord, player):
-    ttt_field[coord[0]][coord[1]] = player_sign[player]
+    if coord[1]:
+        ttt_field[coord[0]][coord[1]] = PLAYER_SIGN[player]
+        return True
+    else:
+        return False
 
 def change_player(player):
     return 1 - player
@@ -25,8 +40,10 @@ ttt_field = [['-' for c in range(3)] for r in range(3)]
 player = 1
 while line_is_done():
     player = change_player(player)
-    set_field(get_move(),player)
+    if not set_field(get_move(), player):
+        break
     show_field()
+else:
+    print("Player '", PLAYER_SIGN(player), "' wins!")
 
-print("Player", player_sign(player), "wins!")
 
