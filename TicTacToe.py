26f62c956828882e7d7275f84607c68d1bdba1fe
Vladislav <komarov_v_y@ngs.ru>
2021-02-20 21:06:25 +0700

    Code line_is_done() and is_win_line()

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,6 +2,8 @@ PLAYER_SIGN = ('x', 'o', '-')
 ATTEMPT_MAX = 3
 FIELD_SIZE_X = 4
 FIELD_SIZE_Y = 5
+WIN_LINE_LEN = 3
+
 
 def show_field(fld):
     print(" ", end='')
@@ -11,15 +13,49 @@ def show_field(fld):
     for n, row in zip(range(len(fld)), fld):
         print(n, *row)
 
-def check_eq_val(L):
-    return True
 
-def line_is_done():
-    # for i in range(;):
-    #     check_eq_val(ttt_field[i])
-    #     check_eq_val([l for l in ttt_field[0:-1][i]])
-    #
-    return True
+def is_win_len(L):
+    while len(L) - WIN_LINE_LEN >=0:
+        same_len = 1
+        char = L.pop()
+        if char == PLAYER_SIGN[2]:
+            continue
+        while L and L[-1] == char:
+            same_len += 1
+            L.pop()
+        if same_len >= WIN_LINE_LEN:
+            return True
+    return False
+
+
+def line_is_done(fld):
+    col_num = len(fld[0])
+    row_num = len(fld)
+    # rows check
+    for row in range(row_num):
+        if is_win_len([fld[row][col] for col in range(col_num)]):
+            return True
+    # columns check
+    for col in range(col_num):
+        if is_win_len([fld[row][col] for row in range(row_num)]):
+            return True
+    #\ diagonal
+    for row in range(row_num):
+        if is_win_len([fld[row+i][i] for i in range(min(row_num-row, col_num))]):
+            return True
+    for col in range(1,col_num):
+        if is_win_len([fld[i][col+i] for i in range(min(row_num, col_num-col))]):
+            return True
+    #/ diagonal
+    for row in range(row_num):
+        if is_win_len([fld[row-i][i] for i in range(min(row+1, col_num))]):
+            return True
+    for col in range(1,col_num):
+        if is_win_len([fld[row_num-1-i][col+i] for i in range(min(row_num, col_num-col))]):
+            return True
+
+    return False
+
 
 def get_move():
     attempt_count = ATTEMPT_MAX + 1
@@ -45,12 +81,14 @@ def get_move():
     # print(f"Your choise {row},{col}")
     return row, col
 
-def set_field(coord, player):
-    if coord[1] is None:
+
+def set_field(fld, coord, player):
+    if coord[0] is None:
         return False
     else:
-        ttt_field[coord[0]][coord[1]] = PLAYER_SIGN[player]
+        fld[coord[0]][coord[1]] = PLAYER_SIGN[player]
         return True
+
 
 def change_player(player):
     return 1 - player
@@ -59,16 +97,17 @@ def change_player(player):
 print("Hello! Let's play TicTacToe!")
 print("Enter row, column coordinates to make a move")
 
+
 # ttt_field = [[(r+1)*10+c+1 for c in range(FIELD_SIZE_X)] for r in range(FIELD_SIZE_Y)]
 ttt_field = [[PLAYER_SIGN[2] for c in range(FIELD_SIZE_X)] for r in range(FIELD_SIZE_Y)]
 player = 1
-while line_is_done():
+while not line_is_done(ttt_field):
     player = change_player(player)
-    if not set_field(get_move(), player):
+    if not set_field(ttt_field, get_move(), player):
         break
     show_field(ttt_field)
 else:
-    print("Player '", PLAYER_SIGN(player), "' wins!")
+    print("Player '", PLAYER_SIGN[player], "' wins!", sep= '')
 
 #TODO code line_is_done()
 
